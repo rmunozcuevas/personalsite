4004d8bded0cacdbbb4a91ae2d153d575820cc70
Raymundo <raymundomc0205@icloud.com>
2025-11-09 00:55:50 -0500

    updated modernpage.html

---
+++ scripts/spot.js
@@ -0,0 +1,14 @@
+const color = document.getElementById('base');
+
+// tried to do .color (only affect text elements), for reference .backgroundColor is what is able to change the actual background color
+// element.style.color → text color
+
+// element.style.backgroundColor → background color
+
+
+function changeColor() {
+        alert("button was clicked")
+        color.style.backgroundColor="#556B2F";
+
+}
+
